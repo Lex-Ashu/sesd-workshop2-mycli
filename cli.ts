e635c55fe332acf7ea5f4740755e9cbef2fff56e
Lex-Ashu <thisisashuchoudhary@gmail.com>
2026-03-04 17:45:30 +0000

    mycli

---
+++ cli.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import { GreetCommand } from "./commands/GreetCommand";
+import { MathCommand } from "./commands/MathCommand";
+import { JokeCommand } from "./commands/JokeCommand";
+import { GithubCommand } from "./commands/GithubCommand";
+import { QuoteCommand } from "./commands/QuoteCommand";
+import { FileInfoCommand } from "./commands/FileInfoCommand";
+import { DogCommand } from "./commands/DogCommand";
+import { AdviceCommand } from "./commands/AdviceCommand";
+
+const program = new Command();
+
+program
+  .name("mycli")
+  .description("CLI tool for SESD Workshop 2")
+  .version("1.0.0");
+
+// Create instances of command classes
+new GreetCommand().register(program);
+new MathCommand().register(program);
+new JokeCommand().register(program);
+new GithubCommand().register(program);
+new QuoteCommand().register(program);
+new FileInfoCommand().register(program);
+new DogCommand().register(program);
+new AdviceCommand().register(program);
+
+program.parse();
---
+++ dist/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+const GreetCommand_1 = require("./commands/GreetCommand");
+const MathCommand_1 = require("./commands/MathCommand");
+const JokeCommand_1 = require("./commands/JokeCommand");
+const GithubCommand_1 = require("./commands/GithubCommand");
+const QuoteCommand_1 = require("./commands/QuoteCommand");
+const FileInfoCommand_1 = require("./commands/FileInfoCommand");
+const DogCommand_1 = require("./commands/DogCommand");
+const AdviceCommand_1 = require("./commands/AdviceCommand");
+const program = new commander_1.Command();
+program
+    .name("mycli")
+    .description("CLI tool for SESD Workshop 2")
+    .version("1.0.0");
+// Create instances of command classes
+new GreetCommand_1.GreetCommand().register(program);
+new MathCommand_1.MathCommand().register(program);
+new JokeCommand_1.JokeCommand().register(program);
+new GithubCommand_1.GithubCommand().register(program);
+new QuoteCommand_1.QuoteCommand().register(program);
+new FileInfoCommand_1.FileInfoCommand().register(program);
+new DogCommand_1.DogCommand().register(program);
+new AdviceCommand_1.AdviceCommand().register(program);
+program.parse();
